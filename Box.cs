7e8219f6d85f6d8f814c4a436d7ca49aa702cd3b
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-12-19 18:48:47 +0200

    Add files via upload

---
+++ Box.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Box_Data
+{
+    internal class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double l, double w, double h)
+        {
+            this.Length = l;
+            this.Width = w;
+            this.Height = h;
+        }
+        public double Length
+        {
+            get { return length; }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("Cannot be less than 1");
+                }
+                else
+                {
+                    length = value;
+                }
+            }
+        }
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Cannot be less than 1");
+                }
+                else
+                {
+                    width = value;
+                }
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Cannot be less than 1");
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2* this.width * this.length + 2 * this.height * this.length + 2* this.height * this.width;
+        }
+        public double LateralSurfaceArea()
+        {
+            return 2 * this.height * (this.width + this.length);
+        }
+        public double Volume()
+        {
+            return this.width * this.length * this.height;
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Class_Box_Data;
+using System;
+
+namespace ClassBoxData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Length:");
+            double l = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Width:");
+            double w = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Height:");
+            double h = double.Parse(Console.ReadLine());
+
+            Box box = new Box(1, 1, 1);
+
+            try
+            {
+                box = new Box(l, w, h);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+
+            Console.WriteLine(box.SurfaceArea());
+            Console.WriteLine(box.LateralSurfaceArea());
+            Console.WriteLine(box.Volume());
+        }
+    }
+}
